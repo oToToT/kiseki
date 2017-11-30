f61dd7b206a0ec6acdd1162ec6080191630e0e81
Tommy <ty1208chiang@gmail.com>
2017-11-30 22:27:02 +0800

    Update DisjointSet

--- codes/Data_Structure/DisjointSet.cpp
+++ codes/Data_Structure/DisjointSet.cpp
@@ -1,12 +1,39 @@
 class DJS{
 	private:
-		int arr[N];
+		vector<int> fa, sz, sv;
+		vector<pair<int*, int>> opt;
+		inline void assign(int *k, int v){
+			opt.PB({k, *k});
+			*k = v;
+		}
 	public:
+		inline void init(int n){
+			fa.resize(n);
+			sz.resize(n);
+			for(int i=0;i<n;i++){
+				fa[i] = i;
+				sz[i] = 1;
+			}
+			opt.clear();
+		}
 		int query(int x){
-			while(arr[x]!=x) x=arr[x];
-			return x;
+			if(fa[x] == x) return x;
+			return query(fa[x]);
 		}
-		int merge(int a, int b){
-			arr[query(a)]=query(b);
+		inline void merge(int a, int b){
+			int af = query(a), bf = query(b);
+			if(af == bf) return;
+			if(sz[af] < sz[bf]) swap(af, bf);
+			assign(&fa[bf], fa[af]);
+			assign(&sz[af], sz[af]+sz[bf]);
 		}
-};
+		inline void save(){sv.PB(SZ(opt));}
+		inline void undo(){
+			int ls = sv.back(); sv.pop_back();
+			while(SZ(opt) > ls){
+				pair<int*, int> cur=opt.back();
+				*cur.FF = cur.SS;
+				opt.pop_back();
+			}
+		}
+}
--- codes/Math/Inverse.cpp
+++ codes/Math/Inverse.cpp
@@ -3,6 +3,28 @@
 long long GetInv(long long x, long long k){
 	return qPow(x, k-2);
 }
+
+inline long long Euler(long long x){
+	long long r=1;
+	for(long long i=2;i*i<=x;++i){
+		if(x%i==0){
+			x/=i;
+			r*=(i-1);
+			while(x%i==0){
+				x/=i;
+				r*=i;
+			}
+		}
+	}
+	if(x>1) r*=x-1;
+	return r;
+}
+// x's inverse mod k
+// if k is not prime
+long long GetInv(long long x, long long k){
+	return qPow(x, Euler(k)-1);
+}
+// or extended_gcd(x, k).second
 // if you need [1, x] (most use: [1, k-1]
 void solve(int x, long long k){
 	inv[1] = 1;
