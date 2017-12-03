f2d7b2158919fd9163ef2fa0b108a3fe81b45d64
Tommy <ty1208chiang@gmail.com>
2017-12-03 22:19:23 +0800

    Update Shik Optimization

--- codes/Basic/Pragma.cpp
+++ codes/Basic/Pragma.cpp
@@ -1,3 +1,2 @@
-#pragma GCC optimize("Ofast,no-stack-protector")
-#pragma GCC target("avx,tune=native")
-// or #pragma GCC target ("sse4")
+#pragma GCC optimize("Ofast,no-stack-protector,no-math-errno")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
