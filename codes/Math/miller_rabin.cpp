dfb4e8c7f3fdd29a9792313ddfae5fee34777667
Tommy <ty1208chiang@gmail.com>
2018-01-02 00:31:15 +0800

    Modify Algorithm of primes

--- codes/Math/miller_rabin.cpp
+++ codes/Math/miller_rabin.cpp
@@ -1,35 +1,87 @@
-template<typename T>
-inline T pow(T a,T b,T mod){// a^b mod mod
-	T ret=1;
-	while(b){
-		if(b&1) ret=(ret*a)%mod;
-		b>>=1;
-		a = (a*a)%mod;
-	}
-	return ret%mod;
-}
-int sprp[3]={2,7,61};// for int range
-int llsprp[7]={2,325,9375,28178,450775,9780504,1795265022};// at least unsigned long long
+// n < 4,759,123,141        3 :  2, 7, 61
+// n < 1,122,004,669,633    4 :  2, 13, 23, 1662803
+// n < 3,474,749,660,383          6 :  pirmes <= 13
+// n < 2^64                       7 :
+// 2, 325, 9375, 28178, 450775, 9780504, 1795265022
+// Make sure testing integer is in range [2, n−2] if
+// you want to use magic.
+#include <type_traits>
+#include <cstdint>
+typedef long long lld;
 
-template<typename T>
-inline bool isprime(T n,int *sprp,int num){
-	if(n==2)return 1;
-	if(n<2||n%2==0)return 0;
-	int t=0;
-	T u=n-1;
-	for(;u%2==0;++t)u>>=1;
-	for(int i=0;i<num;++i){
-		T a=sprp[i]%n;
-		if(a==0 or a==1 or a==n-1)continue;
-		T x=pow(a,u,n);
-		if(x==1 or x==n-1)continue;
-		for(int j=0;j<t;++j){
-			x=(x*x)%n;
-			if(x==1)return 0;
-			if(x==n-1)break;
-		}
-		if(x==n-1)continue;
-		return 0;
+// PRNG {{{
+template<class T, T x1, T x2, T x3, int y1, int y2, int y3>
+struct PRNG {
+    using S = typename std::make_signed<T>::type;
+    T s;
+    PRNG(T _s = 0) : s(_s) {}
+    T next() {
+        T z = (s += x1);
+        z = (z ^ (z >> y1)) * x2;
+        z = (z ^ (z >> y2)) * x3;
+        return z ^ (z >> y3);
+    }
+    T next(T n) { return next() % n; }
+    S next(S l, S r) { return l + next(r - l + 1); }
+    T operator()() { return next(); }
+    T operator()(T n) { return next(n); }
+    S operator()(S l, S r) { return next(l, r); }
+    static T gen(T s) { return PRNG(s)(); }
+    template<class U>
+    void shuffle(U first, U last) {
+        size_t n = last - first;
+        for (size_t i = 0; i < n; i++) swap(first[i], first[next(i + 1)]);
+    }
+};
+
+using R32 = PRNG<uint32_t, 0x9E3779B1, 0x85EBCA6B, 0xC2B2AE35, 16, 13, 16>;
+R32 r32;
+
+using R64 = PRNG<uint64_t, 0x9E3779B97F4A7C15, 0xBF58476D1CE4E5B9, 0x94D049BB133111EB, 30, 27, 31>;
+R64 r64;
+// }}}
+
+
+lld mul(lld a, lld b, lld m){
+	// return (a*b)%m;
+	lld ret = 0;
+	while(b){
+		if(b&1) ret = (ret+a)%m;
+		a = (a+a)%m;
+		b>>=1;
+	}
+	return ret;
+}
+
+lld qPow(lld a, lld n, lld m){
+	lld ret = 1;
+	while(n){
+		if(n&1) ret = ret*a%m;
+		n >>= 1;
+		a = a*a%m;
+	}
+	return ret%m;
+}
+bool witness(lld a,lld n,lld u,int t){
+	lld x=qPow(a,u,n);
+	for(int i=0;i<t;i++) {
+		lld nx=mul(x,x,n);
+		if(nx==1&&x!=1&&x!=n-1) return 1;
+		x=nx;
+	}
+	return x!=1;
+}
+bool miller_rabin(lld n,int s=100) {
+	// iterate s times of witness on n
+	// return 1 if prime, 0 otherwise
+	if(n<2) return 0;
+	if(!(n&1)) return n == 2;
+	lld u=n-1; int t=0;
+	// n-1 = u*2^t
+	while(!(u&1)) u>>=1, t++;
+	while(s--){
+		lld a=r64()%(n-1)+1;
+		if(witness(a,n,u,t)) return 0;
 	}
 	return 1;
 }
