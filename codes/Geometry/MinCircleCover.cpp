033263e797fe3ea1d1bfe1cd4ca88d9885da7381
Tommy <ty1208chiang@gmail.com>
2018-06-17 17:41:44 +0800

    Fix Min Covering Circle

--- codes/Geometry/MinCircleCover.cpp
+++ codes/Geometry/MinCircleCover.cpp
@@ -1,18 +1,18 @@
 template<typename T>
 Circle<llf> MinCircleCover(const vector<Point<T>>& pts){
-    random_shuffle(ALL(pt));
-    Circle<llf> c = {pt[0], 0};
+    random_shuffle(ALL(pts));
+    Circle<llf> c = {pts[0], 0};
     int n = SZ(pts);
     for(int i=0;i<n;i++){
-        if(pt[i].in(c)) continue;
-        c = {pt[i], 0};
+        if(pts[i].in(c)) continue;
+        c = {pts[i], 0};
         for(int j=0;j<i;j++){
-            if(pt[j].in(c)) continue;
-            c.o = (pt[i] + pt[j]) / 2;
-            c.r = pt[i].dis(c.o);
+            if(pts[j].in(c)) continue;
+            c.o = (pts[i] + pts[j]) / 2;
+            c.r = pts[i].dis(c.o);
             for(int k=0;k<j;k++){
-                if(pt[k].in(c)) continue;
-                c = get_circum(pt[i], pt[j], pt[k]);
+                if(pts[k].in(c)) continue;
+                c = get_circum(pts[i], pts[j], pts[k]);
             }
         }
     }
