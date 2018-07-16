c4fa327ed396a8610f2591925d1470864603f503
Tommy <ty1208chiang@gmail.com>
2018-07-16 20:32:15 +0800

    Fix Prime Sieve

--- codes/Math/QuickSieve.cpp
+++ codes/Math/QuickSieve.cpp
@@ -1,12 +1,7 @@
-bol notprime[N];
-vector<int> primes;
-
 void Sieve(int n){
-    // reverse true false for quicker
     for(int i=2;i<=n;i++){
-        if(!notprime[i]){
-            primes.push_back(i);
-            for(int j=i*i;j<=n;j+=i) notprime[i]=true;
-        }
+        if(notprime[i]) continue;
+        primes.push_back(i);
+        for(int j=i*i;j<=n;j+=i) notprime[i]=true;
     }
 }
