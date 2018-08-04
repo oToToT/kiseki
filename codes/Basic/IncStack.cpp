eb6159c0963dbe44610227c71d1fd7d51a951e7c
Tommy <ty1208chiang@gmail.com>
2018-08-05 04:23:42 +0800

    Thanks to HackerCup, I know IncStack

--- codes/Basic/IncStack.cpp
+++ codes/Basic/IncStack.cpp
@@ -15,3 +15,22 @@ void increase_stack_size() {
         }
     }
 }
+
+// craziest way
+static void run_with_stack_size(void (*func)(), size_t stsize) {
+    char *stack, *send;
+    stack=(char *)malloc(stsize);
+    send=stack+stsize-16;
+    send=(char *)((uintptr_t)send/16*16);
+    asm volatile(
+      "mov %%rsp, (%0)\n"
+      "mov %0, %%rsp\n"
+      :
+      : "r" (send));
+    func();
+    asm volatile(
+      "mov (%0), %%rsp\n"
+      :
+      : "r" (send));
+    free(stack);
+}
