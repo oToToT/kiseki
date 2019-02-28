f09d0dfd67f0a84536b50085cfacf0413f181f6b
Tommy <ty1208chiang@gmail.com>
2019-02-28 20:12:29 +0800

    fix treap and shorten mcmf

--- codes/Data_Structure/Treap.cpp
+++ codes/Data_Structure/Treap.cpp
@@ -1,83 +1,59 @@
-class Treap{
-private:
-  const int MEM = 500000 + 5;
+namespace Treap{
+  #define sz( x ) ( ( x ) ? ( ( x )->size ) : 0 )
+  #define sm( x ) ( ( x ) ? ( ( x )->sum ) : 0 )
   struct node{
+    int size, cnt, sum;
+    uint32_t pri;
     node *lc, *rc;
-    int pri, size, val;
-    node(){}
-    node(int x): 
-      lc(nullptr), 
-      rc(nullptr),
-      pri(rand()),
-      size(1), 
-      val(x)
-    {}
-    inline void pull(){
+    node(): size( 0 ), cnt( 0 ), sum( 0 ), pri( rand() ),
+            lc( nullptr ), rc( nullptr ) {}
+    node( int x ): size( 1 ), cnt( x ), sum( x ), pri( rand() ),
+            lc( nullptr ), rc( nullptr ) {}
+    void pull() {
+      sum = cnt;
+      if ( lc ) sum += lc->sum;
+      if ( rc ) sum += rc->sum;
       size = 1;
-      if(lc) size += lc->size;
-      if(rc) size += rc->size;
+      if ( lc ) size += lc->size;
+      if ( rc ) size += rc->size;
     }
-  } *root, pool[MEM];
-  int mem_;
-  inline node* new_node(int x){
-    static int mem_ = 0;
-    assert(mem_ < MEM);
-    pool[mem_]=node(x);
-    return &pool[mem_++];
-  }
-  inline int sz(node* x){return x?x->size:0;}
-  node* merge(node *a, node *b){
-    if(!a or !b) return a?a:b;
-    if(a->pri > b->pri){
-      a->rc = merge(a->rc, b);
-      a->pull();
-      return a;
-    }else{
-      b->lc = merge(a, b->lc);
-      b->pull();
-      return b;
+  };
+  node* merge( node* L, node* R ) {
+    if ( not L or not R ) return L ? L : R;
+    if ( L->pri > R->pri ) {
+      L->rc = merge( L->rc, R );
+      L->pull();
+      return L;
+    } else {
+      R->lc = merge( L, R->lc );
+      R->pull();
+      return R;
     }
   }
-  void split(Treap* t, int k, Treap*& a, Treap*& b){
-    if(!t) a=b=nullptr;
-    else if(sz(t->lc) < k){
-      a = t;
-      split(t->rc, k - sz(t->lc) - 1 , a->rc, b);
-      a->pull();
-    }else{
-      b = t;
-      split(t->lc, k, a, b->lc);
-      b->pull();
+  void split_by_size( node* rt, int k, node*& L, node*& R ) {
+    if ( not rt ) L = R = nullptr;
+    else if( sz( rt->lc ) + 1 <= k ) {
+      L = rt;
+      split_by_size( rt->rc, k - sz( rt->lc ) - 1, L->rc, R );
+      L->pull();
+    } else {
+      R = rt;
+      split_by_size( rt->lc, k, L, R->lc );
+      R->pull();
     }
   }
-  int oOk(node* rr, int x){
-    if(rr==NULL)return 0;
-    if((rr->val) < x)return gSize(rr->l)+oOk(rr->r, x)+1;
-    else return oOk(rr->l, x);
+  void split_by_sum( node* rt, int k, node*& L, node*& R ) {
+    if ( not rt ) L = R = nullptr;
+    else if( sm( rt->lc ) + rt->cnt <= k ) {
+      L = rt;
+      split_by_sum( rt->rc, k - sm( rt->lc ) - rt->cnt, L->rc, R );
+      L->pull();
+    } else {
+      R = rt;
+      split_by_sum( rt->lc, k, L, R->lc );
+      R->pull();
+    }
   }
-public:
-  Treap(){root=nullptr;seed=time(NULL);mem_=0;}
-  void do_something_at(int l, int r){
-    // 1-base [l, r]
-    split(root, l-1, tl, root);
-    split(root, r-l+1, root, tr);
-    root = merge(tl, merge(root, tr));
-  }
-  void insert(int x){
-    node *a, *b;
-    split(root, x, a, b);
-    root = merge(merge(a, new node(x)), b);
-    root->size = gSize(root->l)+gSize(root->r)+1;
-  }
-  void remove(int x){
-    //need debug may contain bugs
-    node *a, *b, *c, *d;
-    split(root, x, a, b);
-    a->size = gSize(a->l)+gSize(a->r);
-    split(a, x-1, c, d);
-    root = merge(b, c);
-    root->size = gSize(root->l)+gSize(root->r);
-    delete d;
-  }
-  int order_of_key(int x){return oOk(root,x);}
-};
+  #undef sz
+  #undef sm
+}
