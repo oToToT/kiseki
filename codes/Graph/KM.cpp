3d027adf3be8780d8cfd1d0b4569e7027456dbaf
Tommy <ty1208chiang@gmail.com>
2019-02-28 21:38:12 +0800

    change KM to waynedisonitau123 ver

--- codes/Graph/KM.cpp
+++ codes/Graph/KM.cpp
@@ -1,53 +1,58 @@
 struct KM{
-// Maximum Bipartite Weighted Matching (Perfect Match)
-  static const int MXN = 650;
-  static const lld INF = 2147483647;
-  int n,match[MXN],vx[MXN],vy[MXN];
-  lld edge[MXN][MXN],lx[MXN],ly[MXN],slack[MXN];
-  void init(int _n){
-    n = _n;
-    for(int i=0; i<n; i++) for(int j=0; j<n; j++)
-      edge[i][j] = 0;
+  static constexpr lld INF = 1LL<<60;
+  lld w[N][N], lx[N], ly[N], slack[N];
+  int match[N], n, vx[N], vy[N], step_;
+  void init(int n_){
+    n=n_,step=0;
+    memset(w,0,sizeof(w));
+    memset(lx,0,sizeof(lx));
+    memset(ly,0,sizeof(ly));
+    memset(slack,0,sizeof(slack));
+    memset(match,0,sizeof(match));
+    memset(vx,0,sizeof(vx));
+    memset(vy,0,sizeof(vy));
   }
-  void addEdge(int x, int y, lld w) { edge[x][y] = w; }
-  bool DFS(int x){
-    vx[x] = 1;
-    for (int y=0; y<n; y++){
-      if (vy[y]) continue;
-      if (lx[x]+ly[y] > edge[x][y]){
-        slack[y]=min(slack[y], lx[x]+ly[y]-edge[x][y]);
-      } else {
-        vy[y] = 1;
-        if (match[y] == -1 || DFS(match[y]))
-        { match[y] = x; return true; }
+  void add_edge(int u,int v,lld w){w[u][v]=w;}
+  bool dfs(int x) {
+    vx[x] = step_;
+    for (int i = 0; i < n; ++i) {
+      if (vy[i]==step_) continue;
+      if (lx[x] + ly[i] > w[x][i]) {
+        slack[i] = min(slack[i], lx[x] + ly[i] - w[x][i]);
+        continue;
+      }
+      vy[i] = step_;
+      if (match[i] == -1 || dfs(match[i])) {
+        match[i] = x;
+        return true;
       }
     }
     return false;
   }
-  lld solve(){
-    fill(match,match+n,-1);
-    fill(lx,lx+n,-INF); fill(ly,ly+n,0);
-    for (int i=0; i<n; i++)
-      for (int j=0; j<n; j++)
-        lx[i] = max(lx[i], edge[i][j]);
-    for (int i=0; i<n; i++){
-      fill(slack,slack+n,INF);
-      while (true){
-        fill(vx,vx+n,0); fill(vy,vy+n,0);
-        if ( DFS(i) ) break;
-        lld d = INF; // long long
-        for (int j=0; j<n; j++)
-          if (!vy[j]) d = min(d, slack[j]);
-        for (int j=0; j<n; j++){
-          if (vx[j]) lx[j] -= d;
-          if (vy[j]) ly[j] += d;
-          else slack[j] -= d;
+  lld solve() {
+    fill_n(match, n, -1);
+    fill_n(lx, n, -INF);
+    fill_n(ly, n, 0);
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < n; ++j)
+        lx[i] = max(lx[i], w[i][j]);
+    for (int i = 0; i < n; ++i) {
+      fill_n(slack, n, INF);
+      while (true) {
+        step_++;
+        if (dfs(i)) break;
+        lld dlt = INF;
+        for (int j = 0; j < n; ++j) if (vy[j] != step_)
+          dlt = min(dlt, slack[j]);
+        for (int j = 0; j < n; ++j) {
+          if (vx[j]==step_) lx[j] -= dlt;
+          if (vy[j]==step_) ly[j] += dlt;
+          else slack[j] -= dlt;
         }
       }
     }
-    lld res=0;
-    for (int i=0; i<n; i++)
-      res += edge[match[i]][i];
+    lld res = 0;
+    for (int i = 0; i < n; ++i) res += w[match[i]][i];
     return res;
   }
-}graph;
+} km;
