0cfffacbcb3eec1e73884b3742384123c8576c8b
oToToT <ty1208chiang@gmail.com>
2019-03-17 17:41:58 +0800

    Faster FFT

--- codes/Math/FFT.cpp
+++ codes/Math/FFT.cpp
@@ -1,45 +1,42 @@
 /*
    polynomial multiply:
-   FFT(a, N, true);
-   FFT(b, N, true);
-   for(int i=0;i<MAXN;i++) c[i] = a[i]*b[i];
-   FFT(c, N, false);
+   DFT(a, len);
+   DFT(b, len);
+   for(int i=0;i<len;i++) c[i] = a[i]*b[i];
+   iDFT(c, len);
    yeah~ go result in c
-   (N must be 2^k and >= len(a)+len(b))
+   (len must be 2^k and >= 2*(max(a, b)))
+   Hand written Cplx would be 2x faster
  */
-typedef long double llf;
-typedef complex<llf> cplx;
-const int MAXN = 262144;
-const llf PI = acos((llf)-1);
-
-cplx A[MAXN], B[MAXN], C[MAXN], omega[MAXN+1];
-
-void init_omega(){
-  const cplx I = {0, 1};
-  for(int i=0;i<=MAXN;i++) omega[i] = exp(i*2*PI/MAXN*I);
+Cplx omega[2][N];
+void init_omega(int n) {
+	static constexpr llf PI=acos(-1);
+	const llf arg=(PI+PI)/n;
+	for(int i=0;i<n;++i) 
+		omega[0][i]={cos(arg*i),sin(arg*i)};
+	for(int i=0;i<n;++i)
+		omega[1][i]=conj(omega[0][i]);
 }
-
-void FFT(cplx arr[], int n, bool ori){
-  // n must be 2^k
-  int theta = MAXN / n;
-  for(int len=n;len>=2;len>>=1){
-    int tot = len>>1;
-    for(int i=0;i<tot;i++){
-      cplx omg = omega[ori?i*theta%MAXN:MAXN-(i*theta%MAXN)];
-      for(int j=i;j<n;j+=len){
-        int k = j+tot;
-        cplx x = arr[j] - arr[k];
-        arr[j] += arr[k];
-        arr[k] = omg * x;
-      }
-    }
-    theta = (theta * 2) % MAXN;
-  }
-  int i = 0;
-  for(int j=1;j<n-1;j++){
-    for(int k=n>>1;k>(i^=k);k>>=1);
-    if(j < i) swap(arr[j], arr[i]);
-  }
-  if(ori) return;
-  for(int i=0;i<n;i++) arr[i] /= n;
+void tran(Cplx arr[],int n,Cplx omg[]) {
+	for(int i=0,j=0;i<n;++i){
+		if(i>j)swap(arr[i],arr[j]);
+		for(int l=n>>1;(j^=l)<l;l>>=1);
+	}
+	for (int l=2;l<=n;l<<=1){
+		int m=l>>1;
+		for(auto p=arr;p!=arr+n;p+=l){
+			for(int i=0;i<m;++i){
+				Cplx t=omg[n/l*i]*p[m+i];
+				p[m+i]=p[i]-t;
+				p[i]+=t;
+			}
+		}
+	}
+}
+void DFT(Cplx arr[],int n){
+  tran(arr,n,omega[0]);
+}
+void iDFT(Cplx arr[],int n){
+	tran(arr,n,omega[1]);
+	for(int i=0;i<n;++i)arr[i]/=n;
 }
