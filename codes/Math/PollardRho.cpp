446e93a2bb5c8f4100a262eaa397a7bf1d94a417
oToToT <ty1208chiang@gmail.com>
2019-05-28 11:48:04 +0800

    pollard rho became faster

--- codes/Math/PollardRho.cpp
+++ codes/Math/PollardRho.cpp
@@ -1,32 +1,21 @@
 // does not work when n is prime
-lld modit(lld x,lld mod) {
-  if(x>=mod) x-=mod;
-  //if(x<0) x+=mod;
-  return x;
-}
-lld mult(lld x,lld y,lld mod) {
-  lld s=0,m=x%mod;
-  while(y) {
-    if(y&1) s=modit(s+m,mod);
-    y>>=1;
-    m=modit(m+m,mod);
-  }
-  return s;
-}
-lld f(lld x,lld mod) {
-  return modit(mult(x,x,mod)+1,mod);
-}
-lld pollard_rho(lld n) {
-  if(!(n&1)) return 2;
-  while (true) {
-    lld y=2, x=rand()%(n-1)+1, res=1;
-    for (int sz=2; res==1; sz*=2) {
-      for (int i=0; i<sz && res<=1; i++) {
-        x = f(x, n);
-        res = __gcd(abs(x-y), n);
-      }
-      y = x;
-    }
-    if (res!=0 && res!=n) return res;
-  }
+// return any non-trivial factor
+llu pollard_rho(llu n){
+	static auto f=[](llu x,llu k,llu m){
+		return add(k,mul(x,x,m),m);
+	};
+	if (!(n&1)) return 2;
+	mt19937 rnd(120821011);
+	while(true){
+		llu y=2, yy=y, x=rnd()%n, t=1;
+		for(llu sz=2;t==1;sz<<=1) {
+			for(llu i=0;i<sz;++i){
+				if(t!=1)break;
+				yy=f(yy,x,n);
+				t=gcd(yy>y?yy-y:y-yy,n);
+			}
+			y=yy;
+		}
+		if(t!=1&&t!=n) return t;
+	}
 }
