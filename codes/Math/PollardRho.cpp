2320572d7d2fdefad79b77e28c532ad9754a8908
oToToT <ty1208chiang@gmail.com>
2019-06-05 22:20:54 +0800

    Faster PollardRho

--- codes/Math/PollardRho.cpp
+++ codes/Math/PollardRho.cpp
@@ -1,21 +1,21 @@
 // does not work when n is prime
 // return any non-trivial factor
 llu pollard_rho(llu n){
-	static auto f=[](llu x,llu k,llu m){
-		return add(k,mul(x,x,m),m);
-	};
-	if (!(n&1)) return 2;
-	mt19937 rnd(120821011);
-	while(true){
-		llu y=2, yy=y, x=rnd()%n, t=1;
-		for(llu sz=2;t==1;sz<<=1) {
-			for(llu i=0;i<sz;++i){
-				if(t!=1)break;
-				yy=f(yy,x,n);
-				t=gcd(yy>y?yy-y:y-yy,n);
-			}
-			y=yy;
-		}
-		if(t!=1&&t!=n) return t;
-	}
+  static auto f=[&](llu x,llu k){
+    return add(k,mul(x,x,n),n);
+  };
+  if (!(n&1)) return 2;
+  mt19937 rnd(120821011);
+  while(true){
+    llu y=2,yy=y,x=rnd()%n,t=1;
+    for(llu sz=2;t==1;sz<<=1) {
+      for(llu i=0;i<sz;++i){
+        if(t!=1)break;
+        yy=f(yy,x);
+        t=gcd(yy>y?yy-y:y-yy,n);
+      }
+      y=yy;
+    }
+    if(t!=1&&t!=n) return t;
+  }
 }
