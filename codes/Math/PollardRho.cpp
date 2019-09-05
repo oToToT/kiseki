19b576c2eea00ac2f3eee500988d64d6dd4f5c19
oToToT <ty1208chiang@gmail.com>
2019-09-05 16:00:29 +0800

    fix pollard rho

--- codes/Math/PollardRho.cpp
+++ codes/Math/PollardRho.cpp
@@ -1,8 +1,8 @@
 // does not work when n is prime
 // return any non-trivial factor
 llu pollard_rho(llu n){
-  static auto f=[&](llu x,llu k){
-    return add(k,mul(x,x,n),n);
+  static auto f=[](llu x,llu k,llu m){
+    return add(k,mul(x,x,m),m);
   };
   if (!(n&1)) return 2;
   mt19937 rnd(120821011);
@@ -11,7 +11,7 @@ llu pollard_rho(llu n){
     for(llu sz=2;t==1;sz<<=1) {
       for(llu i=0;i<sz;++i){
         if(t!=1)break;
-        yy=f(yy,x);
+        yy=f(yy,x,n);
         t=gcd(yy>y?yy-y:y-yy,n);
       }
       y=yy;
