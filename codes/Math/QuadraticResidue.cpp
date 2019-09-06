e2851f166cf73655bcda0a6dc9eb1503924c8a32
oToToT <ty1208chiang@gmail.com>
2019-09-06 23:04:20 +0800

    add quadraticresidue

---
+++ codes/Math/QuadraticResidue.cpp
@@ -0,0 +1,38 @@
+struct Status{
+	ll x,y;
+};
+ll w;
+inline Status mult(const Status& a,const Status& b,ll mod){
+	Status res;
+	res.x=(a.x*b.x+a.y*b.y%mod*w)%mod;
+	res.y=(a.x*b.y+a.y*b.x)%mod;
+	return res;
+}
+inline Status qpow(Status _base,ll _pow,ll _mod){
+	Status res;
+	res.x=1,res.y=0;
+	while(_pow>0){
+		if(_pow&1) res=mult(res,_base,_mod);
+		_base=mult(_base,_base,_mod);
+		_pow>>=1;
+	}
+	return res;
+}
+inline ll check(ll x,ll p){
+	return qpow_mod(x,(p-1)>>1,p);
+}
+inline ll get_root(ll n,ll p){
+	if(p==2) return 1;
+	if(check(n,p)==p-1) return -1;
+	ll a;
+	while(true){
+		a=rand()%p;
+		w=((a*a-n)%p+p)%p;
+		if(check(w,p)==p-1) break;
+	}
+	Status res;
+	res.x=a;
+	res.y=1;
+	res=qpow(res,(p+1)>>1,p);
+	return res.x;
+}
