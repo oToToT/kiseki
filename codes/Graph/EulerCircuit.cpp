4abf9bd603cba546f155582d342e23b2b9385bd5
oToToT <ty1208chiang@gmail.com>
2019-10-17 22:56:27 +0800

    add EulerCircuit

---
+++ codes/Graph/EulerCircuit.cpp
@@ -0,0 +1,14 @@
+bool vis[ N ]; size_t la[ K ];
+void dfs( int u, vector< int >& vec ) {
+	if ( la[ u ] == G[ u ].size() ) vec.push_back( u );
+	for ( ; la[ u ] < G[ u ].size() ; ) {
+		if ( vis[ G[ u ][ la[ u ] ].second ] ) {
+			++ la[ u ];
+			continue;
+		}
+		int v = G[ u ][ la[ u ] ].first;
+		vis[ G[ u ][ la[ u ] ].second ] = true;
+		++ la[ u ]; dfs( v, vec );
+		vec.push_back( v );
+	}
+}
