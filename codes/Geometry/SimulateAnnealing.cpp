2ccb51867c8c0ae3f066fd7f6f4f2e3cf16bd6ce
oToToT <ty1208chiang@gmail.com>
2019-10-20 18:56:26 +0800

    update SimulatingAnnealing

--- codes/Geometry/SimulateAnnealing.cpp
+++ codes/Geometry/SimulateAnnealing.cpp
@@ -1,19 +1,22 @@
-double getY(double);
-int main(){
-  int rr, ll;
-  default_random_engine rEng(time(NULL));
-  uniform_real_distribution<double> Range(-1,1);
-  uniform_real_distribution<double> expR(0,1);
-  auto Random=bind(Range,rEng), expRand=bind(expR,rEng);
-  int step=0;
-  double pace=rr-ll, mini=0.95; // need to search for it
-  double x=max(min(Random()*pace+ll, rr), ll), y=getY(x);
-  while(pace>=1e-7){
-    double newX = max(min(x + Random()*pace, rr), ll);
-    double newY = getY(newX);
-    if(newY < y || expRand() < exp(-step))
-      x=newX, y=newY;
-    step++;
-    pace*=mini;
+llf anneal() {
+  mt19937 rnd_engine( seed );
+  uniform_real_distribution< llf > rnd( 0, 1 );
+  const llf dT = 0.001;
+  // Argument p
+  llf S_cur = calc( p ), S_best = S_cur;
+  for ( llf T = 2000 ; T > EPS ; T -= dT ) {
+    // Modify p to p_prime
+    const llf S_prime = calc( p_prime );
+    const llf delta_c = S_prime - S_cur;
+    llf prob = min( ( llf ) 1, exp( -delta_c / T ) );
+    if ( rnd( rnd_engine ) <= prob ) {
+      S_cur = S_prime;
+      p = p_prime;
+    }
+    if ( S_prime < S_best ) {
+      S_best = S_prime;
+      p_best = p_prime;
+    }
   }
+  return S_best;
 }
