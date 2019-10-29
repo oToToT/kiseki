a528de10d4977f0cf41d2327fb206756621120fe
oToToT <ty1208chiang@gmail.com>
2019-10-29 22:35:28 +0800

    add tree hash

---
+++ codes/Graph/TreeHash.cpp
@@ -0,0 +1,9 @@
+uint64_t hsah( int u, int f ) {
+    uint64_t r = 127;
+    for ( int v : G[ u ] ) {
+        if ( v == f ) continue;
+        uint64_t hh = hsah( v, u );
+        r = r + ( hh * hh ) % mod;
+    }
+    return r;
+}
