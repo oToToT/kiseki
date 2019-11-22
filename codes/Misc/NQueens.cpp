01b194657ad5a75eb1e3ddd0efe24067d4d789ac
Tommy <ty1208chiang@gmail.com>
2019-11-23 02:15:22 +0800

    add n queens problem

---
+++ codes/Misc/NQueens.cpp
@@ -0,0 +1,25 @@
+vector< int > solve( int n ) {
+  // no solution when n=2, 3
+  vector< int > ret;
+  if ( n % 6 == 2 ) {
+    for ( int i = 2 ; i <= n ; i += 2 )
+      ret.push_back( i );
+    ret.push_back( 3 ); ret.push_back( 1 );
+    for ( int i = 7 ; i <= n ; i += 2 )
+      ret.push_back( i );
+    ret.push_back( 5 );
+  } else if ( n % 6 == 3 ) {
+    for ( int i = 4 ; i <= n ; i += 2 )
+      ret.push_back( i );
+    ret.push_back( 2 );
+    for ( int i = 5 ; i <= n ; i += 2 )
+      ret.push_back( i );
+    ret.push_back( 1 ); ret.push_back( 3 );
+  } else {
+    for ( int i = 2 ; i <= n ; i += 2 )
+      ret.push_back( i );
+    for ( int i = 1 ; i <= n ; i += 2 )
+      ret.push_back( i );
+  }
+  return ret;
+}
