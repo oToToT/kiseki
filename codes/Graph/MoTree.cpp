40d61a240444359a348a96b0bba4ce8ea631cb52
Tommy <ty1208chiang@gmail.com>
2019-11-23 00:59:35 +0800

    add mo's algorithm on tree

---
+++ codes/Graph/MoTree.cpp
@@ -0,0 +1,71 @@
+int n, q, nxt[ N ], to[ N ], hd[ N ];
+struct Que{
+  int u, v, id;
+} que[ N ];
+void init() {
+  cin >> n >> q;
+  for ( int i = 1 ; i < n ; ++ i ) {
+    int u, v; cin >> u >> v;
+    nxt[ i << 1 | 0 ] = hd[ u ];
+    to[ i << 1 | 0 ] = v;
+    hd[ u ] = i << 1 | 0;
+    nxt[ i << 1 | 1 ] = hd[ v ];
+    to[ i << 1 | 1 ] = u;
+    hd[ v ] = i << 1 | 1;
+  }
+  for ( int i = 0 ; i < q ; ++ i ) {
+    cin >> que[ i ].u >> que[ i ].v;
+    que[ i ].id = i;
+  }
+}
+int dfn[N], dfn_, block_id[N], block_, stk[N], stk_;
+void dfs( int u, int f ) {
+  dfn[ u ] = dfn_++; int saved_rbp = stk_;
+  for ( int v_ = hd[ u ] ; v_ ; v_ = nxt[ v_ ] ) {
+    if ( to[ v_ ] == f ) continue;
+    dfs( to[ v_ ], u );
+    if ( stk_ - saved_rbp < SQRT_N ) continue;
+    for ( ++ block_ ; stk_ != saved_rbp ; )
+       block_id[ stk[ -- stk_ ] ] = block_;
+  }
+  stk[ stk_ ++ ] = u;
+}
+bool inPath[ N ];
+void Diff( int u ) {
+  if ( inPath[ u ] ^= 1 )
+    // remove this edge
+  else
+    // add this edge
+}
+void traverse( int& origin_u, int u ) {
+  for ( int g = lca( origin_u, u ) ; 
+    origin_u != g ; origin_u = parent_of[ origin_u ] )
+      Diff( origin_u );
+  for ( int v = u ; v != origin_u ; v = parent_of[ v ] )
+    Diff( v );
+  origin_u = u;
+}
+void solve() {
+  dfs( 1, 1 );
+  while ( stk_ ) block_id[ stk[ -- stk_ ] ] = block_;
+  sort( que, que + q, [] ( const Que& x, const Que& y ){
+    return tie( block_id[ x.u ], dfn[ x.v ] )
+            < tie( block_id[ y.u ], dfn[ y.v ] );
+  } );
+  int U = 1, V = 1;
+  for ( int i = 0 ; i < q ; ++ i ) {
+    pass( U, que[ i ].u );
+    pass( V, que[ i ].v );
+    // we could get our answer of que[ i ].id
+  }
+}
+/*
+Method 2:
+dfs u:
+  push u
+  iterate subtree
+  push u
+Let P = LCA(u, v), and St(u)<=St(v)
+if (P == u) query[St(u), St(v)]
+else query[Ed(u), St(v)], query[St(P), St(P)]
+*/
