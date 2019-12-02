ea6aafb455289f2a7e7a2193c1c5d20bc00b1125
Tommy <ty1208chiang@gmail.com>
2019-12-02 14:57:03 +0800

    MaxClique by oToToT

--- codes/Graph/MaxClique.cpp
+++ codes/Graph/MaxClique.cpp
@@ -1,67 +1,70 @@
-#define N 111
-struct MaxClique{ // 0-base
-  typedef bitset< N > Int;
-  Int linkto[ N ] , v[ N ];
-  int n;
-  void init( int _n ){
-    n = _n;
-    for( int i = 0 ; i < n ; i ++ ){
-      linkto[ i ].reset();
-      v[ i ].reset();
+// contain a self loop u to u, than u won't in clique
+template < size_t MAXN >
+class MaxClique{
+  private:
+    using bits = bitset< MAXN >;
+    bits popped, G[ MAXN ], ans;
+    size_t deg[ MAXN ], deo[ MAXN ], n;
+    inline void sort_by_degree() {
+      popped.reset();
+      for ( size_t i = 0 ; i < n ; ++ i )
+          deg[ i ] = G[ i ].count();
+      for ( size_t i = 0 ; i < n ; ++ i ) {
+          size_t mi = MAXN, id = 0;
+          for ( size_t j = 0 ; j < n ; ++ j )
+              if ( not popped[ j ] and deg[ j ] < mi )
+                  mi = deg[ id = j ];
+          popped[ deo[ i ] = id ] = 1;
+          for( size_t u = G[ i ]._Find_first() ;
+           u < n ; u = G[ i ]._Find_next( u ) )
+              -- deg[ u ];
+      }
     }
-  }
-  void add_edge( int a , int b ){
-    v[ a ][ b ] = v[ b ][ a ] = 1;
-  }
-  int popcount(const Int& val)
-  { return val.count(); }
-  int lowbit(const Int& val)
-  { return val._Find_first(); }
-  int ans , stk[ N ];
-  int id[ N ] , di[ N ] , deg[ N ];
-  Int cans;
-  void maxclique(int elem_num, Int candi){
-    if(elem_num > ans){
-      ans = elem_num;
-      cans.reset();
-      for( int i = 0 ; i < elem_num ; i ++ )
-        cans[ id[ stk[ i ] ] ] = 1;
+    void BK( bits R, bits P, bits X ) {
+      if ( R.count() + P.count() <= ans.count() )return;
+      if ( not P.count() and not X.count() ) {
+        if ( R.count() > ans.count() ) ans = R;
+        return;
+      }
+      /* greedily chosse max degree as pivot
+      bits cur = P | X; size_t pivot = 0, sz = 0;
+      for ( size_t u = cur._Find_first() ;
+       u < n ; u = cur._Find_next( u ) )
+          if ( deg[ u ] > sz ) sz = deg[ pivot = u ];
+      cur = P & ( ~G[ pivot ] );
+      */ // or simply choose first
+      bits cur = P & ( ~G[ ( P | X )._Find_first() ] );
+      for ( size_t u = cur._Find_first() ; 
+       u < n ; u = cur._Find_next( u ) ) {
+        if ( R[ u ] ) continue;
+        R[ u ] = 1;
+        BK( R, P & G[ u ], X & G[ u ] );
+        R[ u ] = P[ u ] = 0, X[ u ] = 1;
+      }
     }
-    int potential = elem_num + popcount(candi);
-    if(potential <= ans) return;
-    int pivot = lowbit(candi);
-    Int smaller_candi = candi & (~linkto[pivot]);
-    while(smaller_candi.count() && potential>ans){
-      int next = lowbit(smaller_candi);
-      candi[ next ] = !candi[ next ];
-      smaller_candi[next] = !smaller_candi[next];
-      potential --;
-      if(next!=pivot
-        &&!(smaller_candi&linkto[next]).count())
-          continue;
-      stk[elem_num] = next;
-      maxclique(elem_num+1, candi&linkto[next]);
+  public:
+    void init( size_t n_ ) {
+      n = n_;
+      for ( size_t i = 0 ; i < n ; ++ i )
+        G[ i ].reset();
+      ans.reset();
     }
-  }
-  int solve(){
-    for( int i = 0 ; i < n ; i ++ ){
-      id[ i ] = i;
-      deg[ i ] = v[ i ].count();
+    void add_edges( int u, bits S ) { G[ u ] = S; }
+    void add_edge( int u, int v ) {
+      G[ u ][ v ] = G[ v ][ u ] = 1;
     }
-    sort( id , id + n , [&](int id1, int id2){
-          return deg[id1] > deg[id2]; } );
-    for( int i = 0 ; i < n ; i ++ )
-      di[ id[ i ] ] = i;
-    for( int i = 0 ; i < n ; i ++ )
-      for( int j = 0 ; j < n ; j ++ )
-        if( v[ i ][ j ] )
-          linkto[ di[ i ] ][ di[ j ] ] = 1;
-    Int cand; cand.reset();
-    for( int i = 0 ; i < n ; i ++ )
-      cand[ i ] = 1;
-    ans = 1;
-    cans.reset(); cans[ 0 ] = 1;
-    maxclique(0, cand);
-    return ans;
-  }
-} solver;
+    int solve() {
+      sort_by_degree(); // or simply iota( deo... )
+      for ( size_t i = 0 ; i < n ; ++ i )
+        deg[ i ] = G[ i ].count();
+      bits pob, nob = 0; pob.set();
+      for ( size_t i = n ; i < MAXN ; ++ i ) pob[ i ]=0;
+      for ( size_t i = 0 ; i < n ; ++ i ) {
+        size_t v = deo[ i ];
+        bits tmp; tmp[ v ] = 1;
+        BK( tmp, pob & G[ v ], nob & G[ v ] );
+        pob[ v ] = 0, nob[ v ] = 1;
+      }
+      return static_cast< int >( ans.count() );
+    }
+};
