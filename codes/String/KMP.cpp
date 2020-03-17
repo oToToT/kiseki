1f0c5c2cb4621c3fb2c26dec4df599177f958133
oToToT <ty1208chiang@gmail.com>
2020-03-17 17:35:36 +0800

    update KMP from w123

--- codes/String/KMP.cpp
+++ codes/String/KMP.cpp
@@ -1,22 +1,27 @@
-int F[N<<1];
-void KMP(char s1[], char s2[], int n, int m){
-  // make F[] for s1+'\0'+s2;
-  char ss[N<<1];
-  int len = n+m+1;
-  for(int i=0;i<n;i++) ss[i] = s1[i];
-  ss[n] = '\0';
-  for(int i=0;i<m;i++) ss[i+1+n] = s2[i];
-  F[0] = F[1] = 0;
-  for(int i=1;i<len;i++){
-    int j = F[i];
-    while(j > 0 and ss[i]!=ss[j]) j = F[j];
-    F[i+1] = (ss[i]==ss[j]?j+1:0);
+vector<int> kmp(const string &s) {
+  vector<int> f(s.size(), 0);
+  /* f[i] = length of the longest prefix 
+     (excluding s[0:i]) such that it coincides 
+     with the suffix of s[0:i] of the same length */
+  /* i + 1 - f[i] is the length of the 
+     smallest recurring period of s[0:i] */
+  int k = 0;
+  for (int i = 1; i < (int)s.size(); ++i) {
+    while (k > 0 && s[i] != s[k]) k = f[k - 1];
+    if (s[i] == s[k]) ++k;
+    f[i] = k;
   }
-  // just find (F[len2+i] == len2)
-  // i from 1 to len+1 for matching
+  return f;
 }
-/* 
-  [0, i]是個循環字串，且循環節為i-f[i]：
-  if(f[i]>0 and i%(i-f[i])==0)
-  cout << i << " " << i/(i-f[i]) << '\n';
-*/
+vector<int> search(const string &s, const string &t) {
+  // return 0-indexed occurrence of t in s
+  vector<int> f = kmp(t), res;
+  for (int i = 0, k = 0; i < (int)s.size(); ++i) {
+    while(k > 0 && (k == (int)t.size() || s[i] != t[k]))
+      k = f[k - 1];
+    if (s[i] == t[k]) ++k;
+    if (k == (int)t.size())
+      res.push_back(i - t.size() + 1);
+  }
+  return res;
+}
