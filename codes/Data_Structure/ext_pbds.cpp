c355371c3f8881182456b2bb3a3e0dfbd7c61224
oToToT <ty1208chiang@gmail.com>
2020-09-14 20:49:44 +0800

    shorten pbds

--- codes/Data_Structure/ext_pbds.cpp
+++ codes/Data_Structure/ext_pbds.cpp
@@ -8,6 +8,7 @@ using __gnu_pbds::thin_heap_tag;
 template<typename T>
 using pbds_heap=__gnu_pbds::prioity_queue<T,less<T>,\
                                      pairing_heap_tag>;
+// a.join(b), pq.modify(pq.push(10), 87)
 using __gnu_pbds::rb_tree_tag;
 using __gnu_pbds::ov_tree_tag;
 using __gnu_pbds::splay_tree_tag;
@@ -15,20 +16,8 @@ template<typename T>
 using ordered_set = __gnu_pbds::tree<T,\
 __gnu_pbds::null_type,less<T>,rb_tree_tag,\
 __gnu_pbds::tree_order_statistics_node_update>;
+// find_by_order, order_of_key
 template<typename A,typename B>
 using hTable1=__gnu_pbds::cc_hash_table<A,B>;
 template<typename A,typename B>
 using hTable2=__gnu_pbds::gp_hash_table<A,B>;
-int main(){
-  ordered_set<int> ss;
-  ss.insert(1); ss.insert(5);
-  assert(*ss.find_by_order(0)==1);
-  assert(ss.order_of_key(-1)==0);
-  pbds_heap pq1, pq2;
-  pq1.push(1); pq2.push(2);
-  pq1.join(pq2);
-  assert(pq2.size()==0);
-  auto it = pq1.push(87);
-  pq1.modify(it, 19);
-  return 0;
-}
