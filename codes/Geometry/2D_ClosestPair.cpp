f740c86e018f841e873dc393acf697257528013d
oToToT <ty1208chiang@gmail.com>
2020-10-28 15:13:49 +0800

    real O(n log n) 2d closest pair
    Resolve #3

--- codes/Geometry/2D_ClosestPair.cpp
+++ codes/Geometry/2D_ClosestPair.cpp
@@ -1,19 +1,20 @@
-struct Pt{
-  llf x, y, d;
-} arr[N];
-inline llf dis(Pt a, Pt b){
-  return hypot(a.x-b.x, a.y-b.y);
-}
-llf solve(){
-  int cur = rand() % n;
-  for(int i=0;i<n;i++) arr[i].d = dis(arr[cur], arr[i]);
-  sort(arr, arr+n, [](Pt a, Pt b){return a.d < b.d;});
-  llf ans = 1e50;
-  for(int i=0;i<n;i++){
-    for(int j=i+1;j<n;j++){
-      if(arr[j].d - arr[i].d > ans) break;
-      ans = min(ans, dis(arr[i], arr[j]));
-    }
+struct cmp_y {
+  bool operator()(const P& p, const P& q) const {
+    return p.y < q.y;
   }
-  return ans;
+};
+multiset<P, cmp_y> s;
+void solve(P a[], int n) {
+  sort(a, a + n, [](const P& p, const P& q) {
+    return tie(p.x, p.y) < tie(q.x, q.y);
+  });
+  llf d = INF; int pt = 0;
+  for (int i = 0; i < n; ++i) {
+    while (pt < i and a[i].x - a[pt].x >= d)
+      s.erase(s.find(a[pt++]));
+    auto it = s.lower_bound(P(a[i].x, a[i].y - d));
+    while (it != s.end() and it->y - a[i].y < d)
+      d = min(d, dis(*(it++), a[i]));
+    s.insert(a[i]);
+  }
 }
