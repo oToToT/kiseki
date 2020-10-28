39e17087a6fc6851eed83990c8ae006f15d0c441
Tommy Chiang <oToToT@users.noreply.github.com>
2020-10-28 20:32:52 +0800

    simplify stack increasing

--- codes/Basic/IncStack.cpp
+++ codes/Basic/IncStack.cpp
@@ -1,20 +1,6 @@
-//stack resize(change esp to rsp if 64-bit system)
-asm( "mov %0,%%esp\n" ::"g"(mem+10000000) );
-// craziest way
-static void run_stack_sz(void(*func)(),size_t stsize){
-  char *stack, *send;
-  stack=(char *)malloc(stsize);
-  send=stack+stsize-16;
-  send=(char *)((uintptr_t)send/16*16);
-  asm volatile(
-    "mov %%rsp, (%0)\n"
-    "mov %0, %%rsp\n"
-    :
-    : "r" (send));
-  func();
-  asm volatile(
-    "mov (%0), %%rsp\n"
-    :
-    : "r" (send));
-  free(stack);
-}
+const int size = 256 << 20;
+register long rsp asm("rsp");
+char *p = (char*)malloc(size) + size, *bak = (char*)rsp;
+__asm__("movq %0, %%rsp\n"::"r"(p));
+// main
+__asm__("movq %0, %%rsp\n"::"r"(bak));
