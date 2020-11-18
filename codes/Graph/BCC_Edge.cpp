010c410d7e12b4181b678bf237c43822c7716e2b
oToToT <ty1208chiang@gmail.com>
2020-11-19 01:42:56 +0800

    fix bcc edge

--- codes/Graph/BCC_Edge.cpp
+++ codes/Graph/BCC_Edge.cpp
@@ -1,40 +1,36 @@
-class BCC{
-private:
-  vector< int > low, dfn;
-  int cnt;
-  vector< bool > bridge;
-  vector< vector< PII > > G;
-  void dfs( int w, int f ) {
-    low[ w ] = dfn[ w ] = cnt++;
-    for ( auto [ u, t ] : G[ w ] ) {
-      if ( u == f ) continue;
-      if ( dfn[ u ] != 0 ) {
-        low[ w ] = min( low[ w ], dfn[ u ] );
-      }else{
-        dfs( u, w );
-        low[ w ] = min( low[ w ], low[ u ] );
-        if ( low[ u ] > dfn[ w ] ) bridge[ t ] = true;
-      }
-    }
-  }
-public:
-  void init( int n, int m ) {
-    G.resize( n ); cnt = 0;
-    fill( G.begin(), G.end(), vector< PII >() );
-    bridge.clear(); bridge.resize( m );
-    low.clear(); low.resize( n );
-    dfn.clear(); dfn.resize( n );
-  }
-  void add_edge( int u, int v ) {
-    // should check for multiple edge
-    G[ u ].emplace_back( v, cnt );
-    G[ v ].emplace_back( u, cnt ++ );
-  }
-  void solve(){
-    cnt = 1;
-    for (int i = 0; i < n; ++i)
-      if (not vis[ i ]) dfs(i, i);
-  }
-  // the id will be same as insert order, 0-base
-  bool is_bridge( int x ) { return bridge[ x ]; }
-} bcc;
+class BCC_Bridge {
+	private:
+		int n, ecnt;
+		vector<vector<pair<int,int>>> G;
+		vector<int> dfn, low;
+		vector<bool> bridge;
+		void dfs(int u, int f) {
+			dfn[u] = low[u] = dfn[f] + 1;
+			for (auto [v, t]: G[u]) {
+				if (v == f) continue;
+				if (dfn[v]) {
+					low[u] = min(low[u], dfn[v]);
+					continue;
+				}
+				dfs(v, u);
+				low[u] = min(low[u], low[v]);
+				if (low[v] > dfn[u]) bridge[t] = true;
+			}
+		}
+	public:
+		void init(int n_) {
+			G.clear(); G.resize(n = n_);
+			low.assign(n, ecnt = 0);
+			dfn.assign(n, 0);
+		}
+		void add_edge(int u, int v) {
+			G[u].emplace_back(v, ecnt);
+			G[v].emplace_back(u, ecnt++);
+		}
+		void solve() {
+			bridge.assign(ecnt, false);
+			for (int i = 0; i < n; ++i)
+				if (not dfn[i]) dfs(i, i);
+		}
+		bool is_bridge(int x) { return bridge[x]; }
+} bcc_bridge;
