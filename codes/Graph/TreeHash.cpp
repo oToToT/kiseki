d3452cb564465b06c53452b0ad1107e3581535cc
Tommy Chiang <oToToT@users.noreply.github.com>
2020-11-19 23:46:42 +0800

    tree hash works in Taipei-Hsinchu 2020 Regional

--- codes/Graph/TreeHash.cpp
+++ codes/Graph/TreeHash.cpp
@@ -2,7 +2,7 @@ uint64_t hsah(int u, int f) {
   uint64_t r = 127;
   for (int v : G[ u ]) if (v != f) {
     uint64_t hh = hsah(v, u);
-    r = r + (hh * hh) % mod;
+    r=(r+(hh*hh)%1010101333)%1011820613;
   }
   return r;
 }
