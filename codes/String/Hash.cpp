7b478fd868ef9e685c94a05cedff03b3986f2140
oToToT <ty1208chiang@gmail.com>
2021-01-30 20:23:49 +0800

    shorten hash.cpp

--- codes/String/Hash.cpp
+++ codes/String/Hash.cpp
@@ -1,19 +1,15 @@
-class Hash{
-private:
-  const int p = 127, q = 1051762951;
-  int sz, prefix[N], power[N];
-  int add(int x, int y){return x+y>=q?x+y-q:x+y;}
-  int sub(int x, int y){return x-y<0?x-y+q:x-y;}
-  int mul(int x, int y){return 1LL*x*y%q;}
-public:
-  void init(const string &x){
-    sz = x.size();prefix[0]=0;power[0]=1;
-    for(int i=1;i<=sz;i++)
-      prefix[i]=add(mul(prefix[i-1], p), x[i-1]);
-    for(int i=1;i<=sz;i++)power[i]=mul(power[i-1], p);
-  }
-  int query(int l, int r){
-    // 1-base (l, r]
-    return sub(prefix[r], mul(prefix[l], power[r-l]));
-  }
+class Hash {
+  private:
+    static constexpr int P = 127, Q = 1051762951;
+    vector<int> h, p;
+  public:
+    void init(const string &s){
+      h.assign(s.size(), 0); p.resize(s.size());
+      for (size_t i = 0; i < s.size(); ++i)
+        h[i + 1] = add(mul(h[i], P), s[i]);
+      generate(p.begin(), p.end(),
+          [x=1, this]()mutable{return x=mul(x, P);});
+    }
+    int query(int l, int r){ // 1-base (l, r]
+      return sub(h[r], mul(h[l], p[r-l]));}
 };
